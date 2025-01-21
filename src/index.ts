7ea56301f59d3847b6e365e89f3b15e33fccac89
damianross <damian.mross@yahoo.com>
2025-01-21 03:15:09 +0200

    express working added some samples for api endpoints for workers

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,56 @@
-import { PrismaClient } from '@prisma/client'
+import { Prisma, PrismaClient } from '@prisma/client'
+import express from 'express'
 
 const prisma = new PrismaClient()
+const app = express()
 
-async function main() {
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-}
+app.get('/workers', async (req, res) => {
+  const users = await prisma.worker.findMany()
+  res.json(users)
+})
 
-  main()
-    .then(async () => {
-      await prisma.$disconnect()
-    })
-    .catch(async (e) => {
-      console.error(e)
-      await prisma.$disconnect()
-      process.exit(1)
-    })
+app.get('/workers/:id', async (req, res) => {
+  const { id } = req.params
+  const user = await prisma.worker.findUnique({
+    where: {
+      id: Number(id),
+    },
+  })
+  res.json(user)
+})
+
+app.post('/workers', async (req, res) => {
+  const { name, phone, email } = req.body
+  const user = await prisma.worker.create({
+    data: {
+      name,
+      phone,
+      email,
+    },
+  })
+  res.json(user)
+})
+
+app.put('/workers/:id', async (req, res) => {
+  const { id } = req.params
+  const { name, phone, email } = req.body
+  const user = await prisma.worker.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      name,
+      phone,
+      email,
+    },
+  })
+  res.json(user)
+})
+
+
+const server = app.listen(3000, () =>
+  console.log(`🚀 Server ready at: http://localhost:3000`),
+)
